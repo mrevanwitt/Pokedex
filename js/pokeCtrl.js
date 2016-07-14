f55797bbcef4b345607dab98d63e422e1c730899
mrevanwitt <mrevanwitt@gmail.com>
2016-07-13 19:49:27 -0600

    updated pokedex

--- js/pokeCtrl.js
+++ js/pokeCtrl.js
@@ -1,8 +1,19 @@
 angular.module('pokedex')
   .controller('pokeCtrl', function ($scope, pokeServ) {
 
-//manipulate api data here
+    $scope.pokeData = function(num) {
+      pokeServ.getPokemon(num).then(function(response){
+        console.log(response);
 
+        $scope.pokePic = response.sprites.front_shiny;
+        $scope.pokeType = response.types[0].type.name;
+        $scope.pokeName = response.name;
+        $scope.pokeId = response.id;
+        //$scope.moves = response.moves;
+        $scope.height = response.height;
+        $scope.weight = response.weight;
+        $scope.exp = response.base_experience
 
-
+      });
+    };
   });
--- js/pokeServ.js
+++ js/pokeServ.js
@@ -1,7 +1,14 @@
 angular.module('pokedex')
-  .service('pokeServ', function () {
-    //make api call to 
+  .service('pokeServ', function ($http) {
 
-
-
+    //http://pokeapi.co/api/v2/
+    this.getPokemon = function (num) {
+      return $http({
+        method: 'GET',
+        url: 'http://pokeapi.co/api/v2/pokemon/' + num + '/'
+      }).then(function(response){
+        console.log(response);
+        return response.data;
+      });
+    };
   });
